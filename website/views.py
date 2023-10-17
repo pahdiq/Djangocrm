b93255909b62ff7244eae6c89cf6b4fcabd28791
Coit Nelson <pahdiq5959@gmail.com>
2023-10-17 11:41:24 -0600

    commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
+
